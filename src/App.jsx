7bdd85477197f8cecc286354d2fac68cbfff31fd
H-miracle <deboimiracle@gmail.com>
2022-10-24 15:06:32 +0000

    theme settings added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './App.css'
+import {ColorModeContext, useMode} from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import Topbar from './scenes/global/Topbar'
 
 const App = () => {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div>App</div>
+    <ColorModeContext.Provider value={colorMode}> 
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app">
+          <main className='content'>
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
