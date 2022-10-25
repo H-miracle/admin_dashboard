4cf39752c6b5ccba958f6e5693fb7df2b10299a1
H-miracle <deboimiracle@gmail.com>
2022-10-25 15:31:45 +0000

    topbar done

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,8 +1,89 @@
-import React from 'react'
+
+import { MenuItem, Menu, ProSidebar } from 'react-pro-sidebar'
+import { useState } from 'react'
+import { Box, IconButton, Typography, useTheme } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { tokens } from '../../theme'
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+
+
+
 
 const Sidebar = () => {
+
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [selected, setSelected] = useState('Dashboard')
+
+
   return (
-    <div>Sidebar</div>
+    <Box 
+      sx={{
+        '& .pro-sidebar-inner': {
+          background: `${colors.primary[400]} !important`
+        },
+        '& .pro-icon-wrapper': {
+          backgroundColor: 'transparent !important'
+        },
+        '& .pro-inner-item': {
+          padding: '5px 35px 5px 20px !important'
+        },
+        '& .pro-inner-item:hover': {
+          color: '#868dfb !important'
+        },
+        '& .pro-menu-item.active': {
+          color: '#6870fa !important'
+        }
+      }}
+      >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape='square'>
+          {/* LOGO AND MENU ICON */}
+          <MenuItem 
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon ={isCollapsed ? <MenuOutlinedIcon/> : undefined}
+            style={{
+              margin: '10px 0 20px 0',
+              color: colors.grey[100]
+            }}
+          >
+            {!isCollapsed && (
+              <Box 
+                display='flex'
+                justifyContent='space-between'
+                alignItems='center'
+                ml='15px'
+              >
+                <Typography variant='h3' color={colors.grey[100]}>
+                  ADMIN
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <CloseOutlinedIcon/>
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+            
+          {/* USER */}
+          
+
+        </Menu>
+      </ProSidebar>
+
+
+    </Box>
   )
 }
 
